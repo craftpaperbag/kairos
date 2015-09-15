87df06aee261a9594ecb560fac39b66f929ed680
craftpaperbag <kazu11518@gmail.com>
2015-09-16 00:21:39 +0900

    rails skelton - first feature

--- spec/features/top_page_spec.rb
+++ spec/features/top_page_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
-describe 'Top page' do
-  it 'runs specs' do
-    true
+describe 'トップページ' do
+  before do
+    visit root_path
   end
+
+  it 'kairosと表示' do
+    expect(page).to have_content 'kairos'
+  end
+
 end
